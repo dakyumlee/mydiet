f3183ae00895750b2c0536957ae3b39bd7e7e6b7
dakyumlee <oicrcutie@gmail.com>
2025-07-30 15:39:55 +0900

    fix: import Controller, GetMapping

--- backend/src/main/java/com/mydiet/controller/HomeController.java
+++ backend/src/main/java/com/mydiet/controller/HomeController.java
@@ -1,7 +1,14 @@
+package com.mydiet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class HomeController {
+
     @GetMapping("/")
     public String redirectToIndex() {
         return "forward:/index.html";
     }
+
 }
