865842a6b610209ae641c608b230a73bceb26990
dakyumlee <oicrcutie@gmail.com>
2025-08-05 15:47:01 +0900

    kakao login

--- backend/src/main/java/com/mydiet/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/mydiet/service/CustomOAuth2UserService.java
@@ -15,26 +15,62 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-        
-        String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        Map<String, Object> attributes = new HashMap<>(oAuth2User.getAttributes());
-        
-        if ("kakao".equals(registrationId)) {
-            Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
-            Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+        try {
+            OAuth2User oAuth2User = super.loadUser(userRequest);
             
-            attributes.put("email", kakaoAccount.get("email"));
-            attributes.put("name", profile.get("nickname"));
-            attributes.put("id", attributes.get("id").toString());
+            String registrationId = userRequest.getClientRegistration().getRegistrationId();
+            Map<String, Object> attributes = new HashMap<>(oAuth2User.getAttributes());
             
-            return new DefaultOAuth2User(
-                oAuth2User.getAuthorities(),
-                attributes,
-                "id"
-            );
+            System.out.println("=== OAuth2 Debug Info ===");
+            System.out.println("Registration ID: " + registrationId);
+            System.out.println("Attributes: " + attributes);
+            
+            if ("kakao".equals(registrationId)) {
+                try {
+                    Object kakaoAccountObj = attributes.get("kakao_account");
+                    String email = null;
+                    String nickname = "Unknown";
+                    
+                    if (kakaoAccountObj instanceof Map) {
+                        Map<String, Object> kakaoAccount = (Map<String, Object>) kakaoAccountObj;
+                        
+                        email = (String) kakaoAccount.get("email");
+                        
+                        Object profileObj = kakaoAccount.get("profile");
+                        if (profileObj instanceof Map) {
+                            Map<String, Object> profile = (Map<String, Object>) profileObj;
+                            nickname = (String) profile.get("nickname");
+                        }
+                    }
+                    
+                    String id = String.valueOf(attributes.get("id"));
+                    attributes.put("email", email != null ? email : id + "@kakao.local"); // 이메일이 없으면 임시 이메일
+                    attributes.put("name", nickname != null ? nickname : "카카오유저");
+                    attributes.put("id", id);
+                    
+                    System.out.println("카카오 처리 완료 - ID: " + id + ", 닉네임: " + nickname + ", 이메일: " + attributes.get("email"));
+                    
+                } catch (Exception e) {
+                    System.err.println("카카오 정보 처리 중 에러: " + e.getMessage());
+                    String id = String.valueOf(attributes.get("id"));
+                    attributes.put("email", id + "@kakao.local");
+                    attributes.put("name", "카카오유저");
+                    attributes.put("id", id);
+                }
+                
+                return new DefaultOAuth2User(
+                    oAuth2User.getAuthorities(),
+                    attributes,
+                    "id"
+                );
+            }
+             
+            return oAuth2User;
+            
+        } catch (Exception e) {
+            System.err.println("OAuth2 처리 중 에러: " + e.getMessage());
+            e.printStackTrace();
+            throw new OAuth2AuthenticationException("OAuth2 처리 실패: " + e.getMessage());
         }
-        
-        return oAuth2User;
     }
 }
